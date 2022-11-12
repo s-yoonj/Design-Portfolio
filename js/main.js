f1a8bb5eabca1bfd7530e168ac792f14801f5e53
s_yoonj <s20111212@gmail.com>
2022-11-12 14:35:35 +0900

    main.js 수정

--- js/main.js
+++ js/main.js
@@ -3,12 +3,18 @@ const content =
 const text = document.querySelector(".text");
 let i = 0;
 
+function sleep(delay) {
+  const start = new Date().getTime();
+  while (new Date().getTime() < start + delay);
+}
+
 function typing() {
   let txt = content[i++];
   text.innerHTML += txt === "\n" ? "<br/>" : txt;
   if (i > content.length) {
     text.textContent = "";
     i = 0;
+    sleep(3000);
   }
 }
 setInterval(typing, 200);
